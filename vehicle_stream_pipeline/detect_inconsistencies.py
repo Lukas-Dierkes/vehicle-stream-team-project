42f271569ab7b7a8777c7c847c17266a04338a03
Marius Jost <marius.jost1@gmail.com>
2022-04-21 10:25:22 +0200

    changed functions

--- vehicle_stream_pipeline/detect_inconsistencies.py
+++ vehicle_stream_pipeline/detect_inconsistencies.py
@@ -3,23 +3,27 @@ from numpy import NaN
 
 
 # check functions should be later transported to seperate .py file
-def check_completed(row):
-    # create empty errorlist
-    errorList = pd.Series({"Errorcode": "0", "Errormessage": ""})
+def check_completed(row, errorlist):
 
     # check price offe and pickup at
     error_expression = (row["price_offer"] != NaN) and (row["pickup_at"] == NaN)
     if ~error_expression:
-        if errorList["Errorcode"] == "0":
-            errorList["Errorcode"] = "1"
+        if errorlist["Errorcode"] == "0":
+            errorlist["Errorcode"] = "1"
         else:
-            errorList["Errorcode"] = errorList["Errorcode"] + "; " + "1"
-        errorList["Errormessage"] = (
-            errorList["Errormessage"] + "; Price_offer, but no pickup_at"
+            errorlist["Errorcode"] = errorlist["Errorcode"] + "; " + "1"
+        errorlist["Errormessage"] = (
+            errorlist["Errormessage"] + "; Price_offer, but no pickup_at"
         )
 
     # return final errorlist
-    return errorList
+    return errorlist
+
+
+def check_timestamp_order(row, errorlist):
+
+    # return final errorlist
+    return errorlist
 
 
 df = pd.read_csv(
@@ -32,6 +36,8 @@ print(df)
 
 # create empty error_df - later used to attach to row - errocode 0 can be filtered out as last step
 error_df = pd.DataFrame()
+# create empty errorlist
+errorlist = pd.Series({"Errorcode": "0", "Errormessage": ""})
 
 for index, row in df.iterrows():
     # infinite loop protection - can be commented out later
@@ -43,14 +49,16 @@ for index, row in df.iterrows():
     # use check function depending on status
     current_status = row["state"]
     if current_status == "completed":
-        errorlist = check_completed(row)
+        errorlist = check_completed(row, errorlist)
+        errorlist = check_timestamp_order(row, errorlist)
     elif current_status != "completed":
         print("not completed")
 
     # add current row +  errorlist to output_df
     error_row = pd.concat(objs=[row, errorlist], axis=0)
     print(error_row)
-    error_df = pd.concat(objs=[error_df, error_row], axis=1)
+    error_df = error_df.append(error_row)
+    # error_df = pd.concat(objs=[error_df, error_row], axis=1)
     print(error_df)
 
 # filter output_df to error rows only (errorcode !=  0)
