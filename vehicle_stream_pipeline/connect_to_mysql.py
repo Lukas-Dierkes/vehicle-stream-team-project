040d46faaa0f9199ba5b7c9437440f455f0c9ff2
Marius Jost <marius.jost1@gmail.com>
2022-05-05 20:23:11 +0200

    function insert_dataframe works - query_dataframe needs to be modified

--- vehicle_stream_pipeline/connect_to_mysql.py
+++ vehicle_stream_pipeline/connect_to_mysql.py
@@ -1,23 +1,44 @@
 # in case of authentication error: pip install mysql-connector-python
-import mysql.connector
+import MySQLdb as my
 import pandas as pd
 
-# open connection to remote MySQL DB
-teamproject_sql = mysql.connector.connect(
-    user="remote_user",
-    password="TeamProject1",
-    host="134.155.95.141",
-    port="3306",
-    database="teamproject",
-)
 
-# queries
-query = "SELECT * FROM testtable "
+class Database:
+    # open connection to remote MySQL DB
+    cnx = my.connect(
+        user="remote_user",
+        password="TeamProject1",
+        host="134.155.95.141",
+        port=3306,
+        database="teamproject",
+    )
 
-# create dataframes from query results
-test_data = pd.read_sql(query, teamproject_sql)
+    # parameter - custom attributes list, custom query
+    def query_dataframe(table, query="SELECT * FROM ", table_attributes="*"):
 
-print(test_data)
+        cnx = Database.cnx
+        # queries
+        query = "SELECT * FROM testtable "
 
-# close connection to remote MySQL DB
-teamproject_sql.close()
+        # create dataframes from query results
+        query_df = pd.read_sql(query, cnx)
+        # close connection to remote MySQL DB
+        cnx.close()
+
+        return query_df
+
+    def insert_dataframe(df, table):
+        cnx = Database.cnx
+        cursor = cnx.cursor()
+
+        df_list = df.values.tolist()
+
+        # create query dynamically from columns names
+        # REQUIRES: df.columns must be dropped upfrond so that df.columnnames have zhe same names as table column names
+        database_attributes = str(tuple(df.columns)).replace("'", "")
+        value_placeholder = str(("%s",) * len(df.columns)).replace("'", "")
+        query = f"INSERT INTO {table} {database_attributes} VALUES {value_placeholder}"
+
+        # executemany https://dev.mysql.com/doc/connector-python/en/connector-python-api-mysqlcursor-executemany.html
+        cursor.executemany(query, df_list)
+        cnx.commit()
