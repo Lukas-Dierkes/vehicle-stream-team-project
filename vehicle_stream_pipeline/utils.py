2605f34829f39f9fe36e7d90c861ba2861ce434f
Lukas Dierkes <dierkeslukas@yahoo.de>
2022-06-30 10:54:46 +0200

    put it in func

--- vehicle_stream_pipeline/utils.py
+++ vehicle_stream_pipeline/utils.py
@@ -1,16 +1,35 @@
-from datetime import datetime as dt
+import collections
 
 import networkx as nx
+import numpy as np
 import pandas as pd
 
 pd.set_option("display.max_columns", None)
 
 
-def get_shortest_ride(df, startpoint, endpoint, start_date, end_date):
-    start_date = dt.strptime(start_date, "%m/%d/%Y")
-    end_date = dt.strptime(end_date, "%m/%d/%Y")
-    days = (end_date - start_date).days + 1
+def get_geo_cordinates_for_path(df_stops, path):
+    stop_latitudes = []
+    stop_longitudes = []
+    for stop in path:
+        stop_latitudes.append(float(df_stops[df_stops["id"] == stop]["latitude"]))
+        stop_longitudes.append(float(df_stops[df_stops["id"] == stop]["longitude"]))
 
+    return (stop_latitudes, stop_longitudes)
+
+
+def calculate_graph(drives):
+    G = nx.from_pandas_edgelist(
+        drives,
+        source="pickup_address",
+        target="dropoff_address",
+        edge_attr="avg_time_to_destination",
+    )
+    return G
+
+
+def calculate_drives(df, start_date, end_date):
+    days = (end_date - start_date).days + 1
+    df["scheduled_to"] = pd.to_datetime(df["scheduled_to"])
     df = df[(df["scheduled_to"] > start_date) & (df["scheduled_to"] < end_date)]
 
     drives = pd.DataFrame(
@@ -32,22 +51,82 @@ def get_shortest_ride(df, startpoint, endpoint, start_date, end_date):
 
     drives["avg_time_to_destination"] = drives["waiting_time"] + drives["avg_ride_time"]
 
-    G = nx.from_pandas_edgelist(
+    return drives
+
+
+def get_shortest_ride(startpoint, endpoint, graph):
+    if startpoint not in graph:
+        return "Not in graph"
+    elif endpoint not in graph:
+        return "Not in graph"
+    else:
+        path = nx.shortest_path(
+            graph, source=startpoint, target=endpoint, weight="avg_time_to_destination"
+        )
+        shortest_time = nx.shortest_path_length(
+            graph,
+            source=startpoint,
+            target=endpoint,
+            weight="avg_time_to_destination",
+            method="dijkstra",
+        )
+
+        return (path, shortest_time)
+
+
+def get_hotspots(df_edges, drives, n=10):
+    graph = calculate_graph(drives)
+    df_edges.rename(columns={"Start #": "start_id", "Ende #": "end_id"}, inplace=True)
+    df_edges["Spots"] = df_edges.apply(
+        lambda x: get_shortest_ride(x.start_id, x.end_id, graph)[0], axis=1
+    )
+
+    df_edges = df_edges[df_edges.Spots != "Not in graph"]
+    hotspots = list(df_edges["Spots"])
+    hotspots = [x for xs in hotspots for x in xs]
+    counter = collections.Counter(hotspots)
+    return counter.most_common(n)
+
+
+def add_drone_flights(df_edges, drives, drone_spots=[1008], radius=500):
+    drone_flights = df_edges.iloc[:, :6]
+    drone_flights.drop(["Start Name", "Ende Name", "Route [m]"], axis=1, inplace=True)
+    drone_flights.rename(columns={"Luftlinie [m]": "Luftlinie"}, inplace=True)
+    drone_flights = drone_flights[drone_flights.Luftlinie <= radius]
+
+    drone_flights["avg_time_to_destination"] = (
+        (drone_flights.Luftlinie * 0.12) / 60 / 60 / 24
+    )
+
+    drone_flights = drone_flights[
+        (drone_flights["start_id"].isin(drone_spots))
+        | (drone_flights["end_id"].isin(drone_spots))
+    ]
+
+    drives_w_flights = pd.merge(
         drives,
-        source="pickup_address",
-        target="dropoff_address",
-        edge_attr="waiting_time",
+        drone_flights,
+        left_on=["pickup_address", "dropoff_address"],
+        right_on=["start_id", "end_id"],
+        how="left",
     )
 
-    path = nx.shortest_path(
-        G, source=startpoint, target=endpoint, weight="avg_time_to_destination"
-    )
-    shortest_time = nx.shortest_path_length(
-        G,
-        source=startpoint,
-        target=endpoint,
-        weight="avg_time_to_destination",
-        method="dijkstra",
+    drives_w_flights["avg_time_to_destination"] = np.where(
+        (
+            drives_w_flights["avg_time_to_destination_x"]
+            > drives_w_flights["avg_time_to_destination_y"]
+        ),
+        drives_w_flights.avg_time_to_destination_y,
+        drives_w_flights.avg_time_to_destination_x,
     )
 
-    return (path, shortest_time)
+    return drives_w_flights[
+        [
+            "pickup_address",
+            "dropoff_address",
+            "number_of_drives",
+            "waiting_time",
+            "avg_ride_time",
+            "avg_time_to_destination",
+        ]
+    ]
